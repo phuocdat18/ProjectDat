ea6fb792541129a0539e855ea6e6df314f24a6fd
unknown <nguyenphuocdat18@gmail.com>
2023-02-17 11:07:38 +0700

    baitap11

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-function showMessage(){
-    document.getElementById('messageDiv').innerHTML = 'Xin chào!';
-}
+let count = 20;
+        let fn_1 =1;
+        let fn_2=1;
+        let fn = 0
+        console.log(fn_1);
+        console.log(fn_2);
+        for(let i = 2; i <= 20; i++) {
+            fn = fn_1 + fn_2 ;
+            console.log(fn);
+            fn_1 = fn_2;
+            fn_2 = fn;
+        }
